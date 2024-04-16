79fefa8d9e42a822a31588e8912b65f61088c0fe
Andy <andy@Andys-MacBook-Pro.local>
2024-04-16 14:09:55 -0400

    resolve

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,6 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: ["./setupTests.ts"],
+    setupFiles: [path.resolve(__dirname, "setupTests.ts")],
   },
 });
