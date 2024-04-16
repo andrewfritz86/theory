09b608076cc9f48e4e3b7f51317fab956dc298a3
Andy <andy@Andys-MacBook-Pro.local>
2024-04-16 14:18:01 -0400

    gah

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "node:path";
+import url from "node:url";
 
+// const __dirname = path.resolve(__dirname, "setupTests.ts");
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: [path.resolve(__dirname, "setupTests.ts")],
+    setupFiles: [path.resolve(__dirname, "setUpTests.ts")],
   },
 });
