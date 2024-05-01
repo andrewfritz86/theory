9081227e203918249a6a0b1049173a609b69d6a9
Andy <andy@Andys-MacBook-Pro.local>
2024-05-01 15:02:03 -0400

    Refactor, add challenge mode and toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,23 @@
 import "./App.css";
 
-import { useState, ChangeEvent } from "react";
+import { useState } from "react";
 
-import DropDown from "./components/DropDown/DropDown";
-import Notes from "./components/Notes/Notes";
+import ModeToggle from "./components/ModeToggle/ModeToggle";
+import Review from "./components/Modes/Review";
 
-import { getAccidentalsCount } from "./utils/helpers";
-
-import data from "./data.json";
-
-import { getNotesInActiveScale } from "./utils/helpers";
+import { Mode } from "./types";
 
 function App() {
-  const [activeScaleID, setActiveScaleID] = useState(1);
-
-  const activeScale =
-    data.scales.find((scale) => scale.id === activeScaleID) || data.scales[0];
-
-  const accidentalsCount = getAccidentalsCount(activeScale, data.notes);
-
-  const activeNotes = getNotesInActiveScale(data.notes, activeScale) || [];
-
-  function handleChange(e: ChangeEvent<HTMLSelectElement>) {
-    setActiveScaleID(Number(e.target.value));
-  }
+  const [activeMode, setActiveMode] = useState(Mode.review);
 
   return (
     <div className="p-4 h-screen flex flex-col justify-center">
       <div>
-        <h1 className="text-3xl font-bold text-center mb-16 text-sky-500/100">
+        <h1 className="text-3xl font-bold text-center mb-12 text-sky-500/100">
           Music Theory Practice
         </h1>
-        <div className="flex flex-col justify-evenly">
-          <Notes activeNotes={activeNotes} activeScale={activeScale} />
-          <div className="flex flex-row justify-evenly">
-            <div className="flex">
-              <h4 className="text-lg font-semibold underline mb-8 mr-2 font-sans no-underline text-sky-500/100">
-                Active Scale:
-              </h4>
-              <DropDown scales={data.scales} handleChange={handleChange} />
-            </div>
-            <div className="">
-              <h4 className="text-lg font-semibold text-sky-500/100">
-                Total Accidentals: {accidentalsCount}
-              </h4>
-            </div>
-          </div>
-        </div>
+        <ModeToggle activeMode={activeMode} setActiveMode={setActiveMode} />
+        {activeMode === Mode.review ? <Review /> : <div>foo</div>}
       </div>
     </div>
   );
---
+++ src/components/Modes/Review.tsx
@@ -0,0 +1,41 @@
+import { useState, ChangeEvent } from "react";
+import data from "../../data.json";
+
+import { getNotesInActiveScale } from "../../utils/helpers";
+import { getAccidentalsCount } from "../../utils/helpers";
+
+import Notes from "../Notes/Notes";
+import DropDown from "../DropDown/DropDown";
+
+export default function Review() {
+  const [activeScaleID, setActiveScaleID] = useState(1);
+
+  const activeScale =
+    data.scales.find((scale) => scale.id === activeScaleID) || data.scales[0];
+
+  const accidentalsCount = getAccidentalsCount(activeScale, data.notes);
+
+  const activeNotes = getNotesInActiveScale(data.notes, activeScale) || [];
+
+  function handleChange(e: ChangeEvent<HTMLSelectElement>) {
+    setActiveScaleID(Number(e.target.value));
+  }
+  return (
+    <div className="flex flex-col justify-evenly">
+      <Notes activeNotes={activeNotes} activeScale={activeScale} />
+      <div className="flex flex-row justify-evenly">
+        <div className="flex">
+          <h4 className="text-lg font-semibold underline mb-8 mr-2 font-sans no-underline text-sky-500/100">
+            Scale:
+          </h4>
+          <DropDown scales={data.scales} handleChange={handleChange} />
+        </div>
+        <div className="">
+          <h4 className="text-lg font-semibold text-sky-500/100">
+            Total Accidentals: {accidentalsCount}
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
+}
