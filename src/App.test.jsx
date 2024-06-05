eb5ecbc19d9aaea42e2a3d1a869301e1bc4e5d94
Andy <andy@Andys-MacBook-Pro.local>
2024-06-05 16:30:46 -0400

    Fix tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
 
 import App from "./App";
 
@@ -17,6 +17,19 @@ describe("App", () => {
       render(<App />);
       const accidentalsCount = screen.getByText(/Total Accidentals/i);
       expect(accidentalsCount).toBeInTheDocument();
+    });
+
+    test("A user can toggle to challenge mode and back", () => {
+      render(<App />);
+      const challengeButton = screen.getByText(/Challenge/i);
+      expect(challengeButton).toBeInTheDocument();
+      fireEvent.click(challengeButton);
+      const header = screen.getByText(/Remaining Notes: /i);
+      expect(header).toBeInTheDocument();
+      const reviewButton = screen.getByText(/Review/i);
+      expect(reviewButton).toBeInTheDocument();
+      fireEvent.click(reviewButton);
+      expect(header).not.toBeInTheDocument();
     });
   });
 });
